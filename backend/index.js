6149fe9af46211579294bd427bcaecbe7bcf016d
Srisha22 <srishasrisha82@gmail.com>
2025-06-26 11:57:45 +0530

    frontend updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://authentication-nu-ten.vercel.app/',
+  origin: 'https://authentication-nu-ten.vercel.app',
   credentials: true
 }));
 app.use(express.json());
